7e1474ca263a56c00f1c147d4fa1420873fa7a2b
Kentaro Kuribayashi <kentarok@gmail.com>
2013-03-23 17:43:20 +0900

    Fixed metadata

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             "plenv"
 maintainer       "Kentaro Kuribayashi"
 maintainer_email "kentarok@gmail.com"
 license          "MIT"
-description      "Chef LWRP for plenv."
+description      "Chef cookbook and LWRPs for plenv"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "0.1.0"
 
